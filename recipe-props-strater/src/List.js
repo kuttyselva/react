7dae00bde004a74ef505779fb36e9ed1c8aee659
kuttyselva <kuttyselva57@gmail.com>
2018-12-09 00:43:05 +0530

    final prop

--- recipe-props-strater/src/List.js
+++ recipe-props-strater/src/List.js
@@ -4,26 +4,10 @@ import './List.css'
 
 import PropTypes from 'prop-types';
 export default class List extends Component{
-  static defaultProps={
+  constructor(props){
+    super(props);
+    this.state={
     recipes:[
-      {
-        title:"spaghetti",
-        instructions:"open jar and mix up the food , Eat",
-        ingredients:["pasta","8 cups Water","spaghetti"],
-        img:"https://www.seriouseats.com/recipes/images/2016/08/20160827-cherry-tomato-pasta-13-1500x1125.jpg"
-      },
-      {
-        title:"spaghetti",
-        instructions:"open jar and mix up the food , Eat",
-        ingredients:["pasta","8 cups Water","spaghetti"],
-        img:"https://www.seriouseats.com/recipes/images/2016/08/20160827-cherry-tomato-pasta-13-1500x1125.jpg"
-      },
-      {
-        title:"spaghetti",
-        instructions:"open jar and mix up the food , Eat",
-        ingredients:["pasta","8 cups Water","spaghetti"],
-        img:"https://www.seriouseats.com/recipes/images/2016/08/20160827-cherry-tomato-pasta-13-1500x1125.jpg"
-      },
       {
         title:"spaghetti",
         instructions:"open jar and mix up the food , Eat",
@@ -49,12 +33,13 @@ export default class List extends Component{
         img:"https://www.seriouseats.com/recipes/images/2016/08/20160827-cherry-tomato-pasta-13-1500x1125.jpg"
       }
     ]
-  }
-  static propTypes={
+  };
+}
+   propTypes={
     recipes:PropTypes.arrayOf(PropTypes.object)
   }
   render(){
-const recipes=this.props.recipes.map((r,index)=>(<Recipe key={index}{...r}></Recipe>));
+const recipes=this.state.recipes.map((r,index)=>(<Recipe key={index}{...r}></Recipe>));
 
 return (
   <div class="recipe">
